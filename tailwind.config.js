5e43bb28b6396c11e0b681f44c11c8564fc717b8
Matheus Henrique Reiter <mathenriquereiter@gmail.com>
2022-12-14 09:43:30 -0300

    css converted to tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    colors: {
+      gray: {
+        100: "#eeeeee",
+        200: "#333333",
+        300: "#222222",
+      },
+      red: {
+        100: "#ff4444",
+      },
+    },
+    fontFamily: {
+      lobster: ["Lobster"],
+    },
+    extend: {},
+    plugins: [],
+  },
+};
